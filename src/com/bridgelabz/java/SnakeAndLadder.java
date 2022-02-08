776e25f5afbbf43fd4c20326638cb275d75a16ce
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-08 15:33:00 +0530

    Added : Single Player At Start Postion 0

--- src/com/bridgelabz/java/SnakeAndLadder.java
+++ src/com/bridgelabz/java/SnakeAndLadder.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.java;
 
 public class SnakeAndLadder {
-	public static void main(String[] args) {
-		System.out.println("Welcome Snake and Ladder Program");
-	}
+		static final int Position = 0;
+		public static void main(String[] Args) {
+			
+		}
 
 }
