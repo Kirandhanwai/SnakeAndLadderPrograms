226719b4093327d54301053d618d28c7f53e9cc1
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-08 15:47:37 +0530

    Added : They are No Play,Ladder or Snake

--- src/com/bridgelabz/java/SnakeAndLadder.java
+++ src/com/bridgelabz/java/SnakeAndLadder.java
@@ -3,16 +3,41 @@ package com.bridgelabz.java;
 import java.util.Random;
 
 public class SnakeAndLadder {
-		static int position = 0;
-		public static int diceRoll() {
-			Random random = new Random();
-			int dice = random.nextInt(6)+1;
-			return dice;
+	static int position = 0;
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
+	public static int diceRoll() {
+		Random random = new Random();
+		int dice = random.nextInt(6)+1;
+		return dice;
+	}
+	public static void optionCheck(int dice) {
+		Random random = new Random();
+		int option = random.nextInt(3);
+		if(option == LADDER) {
+			System.out.println("LADDER");
+			position =position + dice;
 		}
-
-		public static void main(String[] args) {
-			System.out.println("dice= "+diceRoll());
-
+		else if(option == SNAKE) {
+			System.out.println("SNAKE");
+			position =position - dice;
 		}
+		else {
+			System.out.println(NO_PLAY);
+			position=position;
+		}
+		if (position<=0)
+			position=0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("dice= "+diceRoll());
+		int dice = diceRoll();
+		System.out.println("dice= "+dice);
+		optionCheck(dice);
+		System.out.println("position = "+position);
+
+	}
 
 }
