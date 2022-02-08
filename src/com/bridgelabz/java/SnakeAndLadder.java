2b035ef15586e84437e3c59f943ba158e6d27222
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-08 15:40:15 +0530

    Dice roll to get number 1 to 6

--- src/com/bridgelabz/java/SnakeAndLadder.java
+++ src/com/bridgelabz/java/SnakeAndLadder.java
@@ -1,9 +1,18 @@
 package com.bridgelabz.java;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
-		static final int Position = 0;
-		public static void main(String[] Args) {
-			
+		static int position = 0;
+		public static int diceRoll() {
+			Random random = new Random();
+			int dice = random.nextInt(6)+1;
+			return dice;
+		}
+
+		public static void main(String[] args) {
+			System.out.println("dice= "+diceRoll());
+
 		}
 
 }
